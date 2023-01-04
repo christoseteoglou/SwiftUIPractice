4ecbd8432c929a67bc8ced097f5cc83051371807
eatoldglue <70583561+christoseteoglou@users.noreply.github.com>
2023-01-04 08:45:16 +0100

    first commit

---
+++ SwiftUIApp/AccountView.swift
@@ -0,0 +1,119 @@
+//
+//  AccountView.swift
+//  SwiftUIApp
+//
+//  Created by Christos Eteoglou on 2023-01-03.
+//
+
+import SwiftUI
+
+struct AccountView: View {
+    @State var isDeleted = false
+    @State var isPinned = false
+    
+    var body: some View {
+        NavigationView {
+            List {
+                profile
+                
+                menu
+                
+                links
+            }
+            .listStyle(.insetGrouped)
+            .navigationTitle("Account")
+        }
+    }
+    
+    var profile: some View {
+        VStack(spacing: 8) {
+            Image(systemName: "person.crop.circle.fill.badge.checkmark")
+                .symbolVariant(.circle.fill)
+                .font(.system(size: 32))
+                .symbolRenderingMode(.palette)
+                .foregroundStyle(.blue, .blue.opacity(0.3))
+                .padding()
+                .background(Circle().fill(.ultraThinMaterial))
+                .background(
+                    HexagonView()
+                        .offset(x: -50, y: -100)
+            )
+            Text("Eatoldglue")
+                .font(.title.weight(.semibold))
+            HStack {
+                Image(systemName: "location")
+                    .imageScale(.small)
+                Text("Sweden")
+                    .foregroundColor(.secondary)
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .padding()
+    }
+    
+    var menu: some View {
+        Section {
+            NavigationLink(destination: ContentView()) {
+                Label("Settings", systemImage: "gear")
+            }
+            NavigationLink { Text("Billing")} label: {
+                Label("Billing", systemImage: "creditcard")
+            }
+            NavigationLink {ContentView()} label: {
+                Label("Help", systemImage: "questionmark")
+            }
+        }
+        .foregroundColor(.primary)
+        .listRowSeparatorTint(.blue)
+        .listRowSeparator(.hidden)
+    }
+    
+    var links: some View {
+        Section {
+            if !isDeleted  {
+                Link(destination: URL(string: "https://christoseteoglou.com")!) {
+                    HStack {
+                        Label("Website", systemImage: "house")
+                        Spacer()
+                        Image(systemName: "link")
+                            .foregroundColor(.secondary)
+                    }
+                }
+                .swipeActions(edge: .leading, allowsFullSwipe: true) {
+                    Button(action: { isDeleted = true }) {
+                        Label("Delete", systemImage: "trash")
+                    }
+                    .tint(.red)
+                    pinActionButton
+                }
+            }
+            Link(destination: URL(string: "https://youtube.com")!) {
+                HStack {
+                    Label("YouTube", systemImage: "tv")
+                    Spacer()
+                    Image(systemName: "link")
+                        .foregroundColor(.secondary)
+                }
+            }
+            .swipeActions(edge: .leading, allowsFullSwipe: true) {
+                pinActionButton
+            }
+        }
+        .foregroundColor(.primary)
+        .listRowSeparator(.hidden)
+    }
+    
+    var pinActionButton: some View {
+        Button { isPinned.toggle() } label: {
+            isPinned ? Label("Unpin", systemImage: "pin.slash") : Label("Pin", systemImage: "pin")
+        }
+        .tint(isPinned ? .gray : .yellow)
+    }
+}
+
+struct AccountView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountView()
+            .preferredColorScheme(.dark)
+    }
+}
---
+++ SwiftUIApp/SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppApp.swift
+//  SwiftUIApp
+//
+//  Created by Christos Eteoglou on 2023-01-03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AccountView()
+        }
+    }
+}
