3742c290ede66eb346288bbabbcea468d0cf121f
eatoldglue <70583561+christoseteoglou@users.noreply.github.com>
2023-01-05 08:25:18 +0100

    BlobView Tab and TabBar work

--- SwiftUIApp/AccountView.swift
+++ SwiftUIApp/AccountView.swift
@@ -38,6 +38,11 @@ struct AccountView: View {
                     HexagonView()
                         .offset(x: -50, y: -100)
             )
+                .background(
+                    BlobView()
+                        .offset(x: 200, y: 0)
+                        .scaleEffect(0.6)
+                )
             Text("Eatoldglue")
                 .font(.title.weight(.semibold))
             HStack {
